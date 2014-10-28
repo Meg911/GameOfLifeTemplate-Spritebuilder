e12ff836d3b7ac58bcebe649cbb0abb24d9b5abb
Meg911 <magreth.degraaf@y7mail.com>
2014-10-28 15:56:06 +1000

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/main.m
+++ GameOfLife.spritebuilder/Source/main.m
@@ -11,19 +11,7 @@
 int main(int argc, char *argv[]) {
     
     @autoreleasepool {
-        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
-        return retVal;
-        
-        
-    }
-}
-/*int main(int argc, char *argv[]) {
-    
-    @autoreleasepool {
         int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
         return retVal;
-        
-        
     }
-}*/
-
+}
